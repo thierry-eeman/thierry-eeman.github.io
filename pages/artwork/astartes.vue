c5a8596b00fe1b6cfc9ef9b2796ddccccfa5bd06
thierry-eeman <thierry.eeman@gmail.com>
2023-06-07 20:44:49 +0200

    fixed bad footer

--- pages/artwork/astartes.vue
+++ pages/artwork/astartes.vue
@@ -1,20 +1,28 @@
 <template>
   <div>
-    <div class="p-10 bg-gradient-to-b from-slate-900 to-slate-800 w-screen min-h-screen">
-      <PageHeader/>
+    <div
+      class="p-10 bg-gradient-to-b from-slate-900 to-slate-800 w-screen min-h-screen"
+    >
+      <PageHeader />
       <div class="grid grid-cols-8 m-5 p-5 gap-3">
-        <h1 class="col-span-8 col-start-6 text-slate-200 text-4xl">Artwork Overview</h1>
+        <h1 class="col-span-8 col-start-6 text-slate-200 text-4xl">
+          Artwork Overview
+        </h1>
       </div>
       <div class="grid grid-cols-2 gap-5 mx-48 margin-auto">
-        <img src="~/assets/images/warhammer.jpg" class="width-full object-contain rounded-2xl shadow-2xl" alt="">
+        <img
+          src="~/assets/images/warhammer.jpg"
+          class="width-full object-contain rounded-2xl shadow-2xl"
+          alt=""
+        />
         <div class="pl-32 text-2xl text-rose-900">
           <h2 class="text-black">Hardware Hero #002</h2>
           <h3 class="text-4xl">Adeptus Astartes</h3>
-          <br>
-          <hr class="border-slate-500">
+          <br />
+          <hr class="border-slate-500" />
+          <PageFooter2 />
         </div>
       </div>
-      <PageFooter />
     </div>
   </div>
 </template>
@@ -23,8 +31,7 @@
 export default {
   name: 'HardwareAstartes',
   data: () => ({}),
-  mounted() {
-  },
-  methods: {}
+  mounted() {},
+  methods: {},
 }
 </script>
